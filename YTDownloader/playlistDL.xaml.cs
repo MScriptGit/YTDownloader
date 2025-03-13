88cc2ef7210b7d6dfb173c122689e040d2f48eac
MScriptGit <marcelcolt@gmail.com>
2025-03-13 09:36:29 +0100

    Added files

---
+++ YTDownloader/playlistDL.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using YoutubeExplode;
+
+namespace YTDownloader
+{
+    public partial class playlistDL : ContentPage
+    {
+        public playlistDL()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void getEntry(object sender, EventArgs e)
+        {
+            //get url
+            string inputText = userInput.Text;
+
+
+
+            var youtube = new YoutubeClient();
+
+            // You can specify either the video URL or its ID
+            var videoUrl = "https://youtube.com/watch?v=u_yIGGhubZs";
+            var video = await youtube.Videos.GetAsync(videoUrl);
+
+            var title = video.Title; // "Collections - Blender 2.80 Fundamentals"
+            var author = video.Author.ChannelTitle; // "Blender"
+            var duration = video.Duration; // 00:07:20
+
+
+
+
+
+            //output??
+            outputTxt.Text = "title";
+        }
+    }
+}

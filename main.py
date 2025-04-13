291c85c0cab56e7496f132a6dc5a3810b0084e2f
MScriptGit <marcelcolt@gmail.com>
2025-04-13 16:14:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
@@ -7,51 +8,51 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 from YTSearch import *
-from YTDownload import *
+from YTDownload import start_download  # Importeer de start_download functie
 from YTPlaylistDL import *
 
 class MyApp(App):
     def build(self):
         float_layout = FloatLayout()
-        
+
         # Create a ScrollView to contain the BoxLayout
         scroll_view = ScrollView(size_hint=(1, None), size=(960, 2400), pos_hint={'top': 1})  # Adjust the height as needed
         self.box_layout = BoxLayout(orientation='vertical', size_hint_y=None)
         self.box_layout.bind(minimum_height=self.box_layout.setter('height'))  # Allow the BoxLayout to grow
-        
+
         # Create Labels
         self.label = Label(text="YTDownloader", font_size=90, size_hint_y=None, height=100)
         self.box_layout.add_widget(self.label)
-        
+
         self.label2 = Label(text="Youtube Downloader", size_hint_y=None, height=100)
         self.box_layout.add_widget(self.label2)
-        
+
         self.label3 = Label(text="", size_hint_y=None, height=20)
         self.box_layout.add_widget(self.label3)
 
         # Create a TextInput widget
         self.text_input = TextInput(hint_text="Voer zoektermen of playlist url in...", size_hint=(None, None), height=100, width=900, pos_hint={'center_x': 0.5}, padding=(20, 20))
         self.box_layout.add_widget(self.text_input)
-        
+
         self.label4 = Label(text="", size_hint_y=None, height=30)
         self.box_layout.add_widget(self.label4)
-        
+
         # Create a BoxLayout for the buttons with horizontal orientation
         button_layout = BoxLayout(orientation='horizontal', spacing=160, size_hint=(None, None), height=100, width=900, pos_hint={'center_x': 0.5})
-        
+
         # Create a search Button widget
         button = Button(text="Zoek!", size_hint=(None, None), height=100, width=300)
         button.bind(on_press=self.on_DLbutton_press)
         button_layout.add_widget(button)
-        
+
         # Create a playlist download Button widget
         buttonPL = Button(text="Download Playlist", size_hint=(None, None), height=100, width=440)
         buttonPL.bind(on_press=self.on_PLbutton_press)
         button_layout.add_widget(buttonPL)
-        
+
         # Add the button layout to the main BoxLayout
         self.box_layout.add_widget(button_layout)
-        
+
         self.label5 = Label(text="", size_hint_y=None, height=30)
         self.box_layout.add_widget(self.label5)
 
@@ -64,8 +65,7 @@ class MyApp(App):
         float_layout.add_widget(scroll_view)
 
         return float_layout
-                
-                
+
     def on_DLbutton_press(self, instance):
         # Get the text from the TextInput
         input_text = self.text_input.text
@@ -79,7 +79,7 @@ class MyApp(App):
         # Create a button for each title and add it to the BoxLayout
         for i, title in enumerate(listTitle):
             button = Button(text=f"{title}\n{listDuration[i]}", size_hint_y=None, height=150)
-            button.bind(on_press=lambda btn, url=listURL[i]: self.on_button_click(url))  # Bind the button to the click event
+            button.bind(on_press=lambda btn, url=listURL[i]: start_download(url))  # Bind the button to the click event
             self.box_layout.add_widget(button)
 
     def clear_previous_buttons(self):
@@ -88,18 +88,13 @@ class MyApp(App):
             if isinstance(child, Button) and child not in [self.label, self.label2, self.label3, self.label4, self.label5, self.labelOutput]:
                 self.box_layout.remove_widget(child)
 
-    def on_button_click(self, url):
-        # Handle the button click event, e.g., start downloading the video
-        YTDownload(url)
-
     def update_label_height(self, instance, value):
         instance.height = instance.texture_size[1]
-        
+
     def on_PLbutton_press(self, instance):
         # Get the text from the TextInput
         input_text = self.text_input.text
         YTPlaylistDL(input_text)
 
-
 if __name__ == "__main__":
-    MyApp().run()
+    MyApp().run()	
